01811c974670eab31e408d814c97b513b4800e13
jonandernovella <novella.uu@gmail.com>
2023-12-19 13:27:06 +0100

    feat: make lib more composable

--- main_test.go
+++ main_test.go
@@ -6,36 +6,44 @@ import (
 	"testing"
 )
 
+func getLib(inputString string) Lib {
+	return Lib{
+		Name:        "darsync",
+		HomeDir:     os.Getenv("HOME"),
+		InputSource: bytes.NewBufferString(inputString),
+	}
+}
+
 func TestGetDirectoryToMove(t *testing.T) {
 	badDir := "/test/dir/bad"
-	lib.InputSource = bytes.NewBufferString(badDir)
-	dirToMove := collectDirectoryToMove()
+	badLib := getLib(badDir)
+	dirToMove := badLib.collectDirectoryToMove()
 	if dirToMove != "" {
 		t.Errorf("getDirectoryToMove() = %s; want %s", dirToMove, badDir)
 	}
 	goodDir := "/dev"
-	lib.InputSource = bytes.NewBufferString(goodDir)
-	dirToMove = collectDirectoryToMove()
+	goodLib := getLib(goodDir)
+	dirToMove = goodLib.collectDirectoryToMove()
 	if dirToMove != goodDir {
 		t.Errorf("getDirectoryToMove() = %s; want %s", dirToMove, goodDir)
 	}
 	file := "/dev/null"
-	lib.InputSource = bytes.NewBufferString(file)
-	dirToMove = collectDirectoryToMove()
+	fileLib := getLib(file)
+	dirToMove = fileLib.collectDirectoryToMove()
 	if dirToMove != "" {
 		t.Errorf("getDirectoryToMove() = %s; want %s", dirToMove, "")
 	}
 }
 func TestGetTargetHost(t *testing.T) {
 	testInput := "test_host"
-	lib.InputSource = bytes.NewBufferString(testInput)
-	host := getTargetHost()
+	testLib := getLib(testInput)
+	host := testLib.getTargetHost()
 	if host != testInput {
 		t.Errorf("getTargetHost() = %s; want %s", host, testInput)
 	}
 	emptyInput := ""
-	lib.InputSource = bytes.NewBufferString(emptyInput)
-	host = getTargetHost()
+	emptyLib := getLib(emptyInput)
+	host = emptyLib.getTargetHost()
 	if host != "dardel.pdc.kth.se" {
 		t.Errorf("getTargetHost() = %s; want %s", host, "dardel.pdc.kth.se")
 	}
@@ -43,8 +51,8 @@ func TestGetTargetHost(t *testing.T) {
 
 func TestGetProjectId(t *testing.T) {
 	goodInput := "good_project_id"
-	lib.InputSource = bytes.NewBufferString(goodInput)
-	projectId := getProjectId()
+	lib := getLib(goodInput)
+	projectId := lib.getProjectId()
 
 	if projectId != goodInput {
 		t.Errorf("getProjectId() = %s; want %s", projectId, goodInput)
@@ -54,14 +62,14 @@ func TestGetProjectId(t *testing.T) {
 
 func TestGetTargetDirectory(t *testing.T) {
 	absolutePath := "/good/target_dir"
-	lib.InputSource = bytes.NewBufferString(absolutePath)
-	targetDir := collectTargetDir("test_host")
+	absLib := getLib(absolutePath)
+	targetDir := absLib.collectTargetDir("test_host")
 	if targetDir != absolutePath {
 		t.Errorf("getTargetDirectory() = %s; want %s", targetDir, absolutePath)
 	}
 	relativePath := "bad_target_dir"
-	lib.InputSource = bytes.NewBufferString(relativePath)
-	targetDir = collectTargetDir("test_host")
+	relLib := getLib(relativePath)
+	targetDir = relLib.collectTargetDir("test_host")
 
 	if targetDir != "" {
 		t.Errorf("getTargetDirectory() = %s; want %s", targetDir, "")
@@ -70,14 +78,14 @@ func TestGetTargetDirectory(t *testing.T) {
 
 func TestGetUsername(t *testing.T) {
 	goodInput := "good_username"
-	lib.InputSource = bytes.NewBufferString(goodInput)
-	username := collectUsername("test_host")
+	goodLib := getLib(goodInput)
+	username := goodLib.collectUsername("test_host")
 	if username != goodInput {
 		t.Errorf("getUsername() = %s; want %s", username, goodInput)
 	}
 	longUsername := "this_is_a_really_long_username_that_is_not_allowed"
-	lib.InputSource = bytes.NewBufferString(longUsername)
-	username = collectUsername("test_host")
+	longLib := getLib(longUsername)
+	username = longLib.collectUsername("test_host")
 	if username != "" {
 		t.Errorf("getUsername() = %s; want %s", username, "")
 	}
@@ -85,14 +93,15 @@ func TestGetUsername(t *testing.T) {
 
 func TestGetPrivateKey(t *testing.T) {
 	goodInput := "/dev/stdout"
-	lib.InputSource = bytes.NewBufferString(goodInput)
-	privateKey := collectPrivateKey()
+	goodLib := getLib(goodInput)
+	privateKey := goodLib.collectPrivateKey()
 	if privateKey != goodInput {
 		t.Errorf("getPrivateKey() = %s; want %s", privateKey, goodInput)
 	}
 	badInput := "~/bad/path"
-	lib.InputSource = bytes.NewBufferString(badInput)
-	privateKey = collectPrivateKey()
+	badLib := getLib(badInput)
+	badLib.InputSource = bytes.NewBufferString(badInput)
+	privateKey = badLib.collectPrivateKey()
 	if privateKey != "" {
 		t.Errorf("getPrivateKey() = %s; want %s", privateKey, "")
 	}
